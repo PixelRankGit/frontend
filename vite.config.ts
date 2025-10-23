fc547d142a57680c84dd015d6be40fbeb0d64253
Edoardo Tombolesi <edoardo@vmi2765050.contaboserver.net>
2025-10-23 18:08:50 +0200

    finalizando vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,9 @@ export default defineConfig({
       "@" : path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: ["dcskw0wko0cwwogg848gog4k.147.93.176.165.sslip.io", "localhost"],
+    port: 8081,
+    host: true
+  },
 })
